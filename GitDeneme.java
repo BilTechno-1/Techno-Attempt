9a08c10c0534a0eb6e04416d04b27d93861cec56
leyla <leylabilcan31@gmail.com>
2024-09-01 22:01:43 +0300

    task 1 yapıldı (bana gelen işin adı Jİradan gelen

---
+++ GitDeneme.java
@@ -0,0 +1,11 @@
+package Git_Attempt;
+
+public class GitDeneme {
+    public static void main(String[] args) {
+        System.out.println("Merhaba");
+
+        System.out.println("Merhaba 2");
+        System.out.println("Task 1 yapıldı");
+
+    }
+}
